ae9277d44c4b83dc6130b9567f2874ac74b25a45
kehuang742 <92164826+kehuang742@users.noreply.github.com>
2023-09-04 15:33:09 +0800

    2-2 & 2-5

---
+++ 20230904/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,43 @@
+﻿
+#include "pch.h"
+#include <iostream>
+using namespace std;
+/*int GCD(int a, int b) {
+	if (a%b == 0) {
+		return b;
+	}
+	else {
+		return GCD(b, a%b);
+	}
+}
+int main()
+{
+	cout<<("Input two numbers: ");
+	int a, b;
+	cin >> a >> b;
+	int result = GCD(a, b);
+	cout << "GCD result" << result;
+	return 0;
+
+}
+*/
+int Factorial(int n) {
+	if (n == 0)
+		return 1;
+	else
+	{
+		return n * Factorial(n - 1);
+	}
+}
+int main()
+{
+	int n ,Total;
+	cout << "請輸入數字";
+	cin >> n;
+	Total = Factorial(n);
+	cout << n << "!=" << Total;
+	return 0;
+}
+
+
+
